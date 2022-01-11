a485013a588e5d0644fda613f2ff50078a23b020
MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
2022-01-11 18:53:38 +0200

    Fixing clock algo.

--- cmake-build-debug/CMakeFiles/PageReplacementAlgorithms.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/PageReplacementAlgorithms.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/PageReplacementAlgorithms.dir/main.c.obj"
-  "PageReplacementAlgorithms.exe"
-  "PageReplacementAlgorithms.exe.manifest"
+  "CMakeFiles/PageReplacementAlgorithms.dir/main.c.o"
+  "PageReplacementAlgorithms"
   "PageReplacementAlgorithms.pdb"
-  "libPageReplacementAlgorithms.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
